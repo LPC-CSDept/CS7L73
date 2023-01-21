dcf3bdad271a100fa2a70a955d2f63d59eb4eeaa
Kyu Lee <kwooler@gmail.com>
2023-01-21 13:21:19 -0800

    L73

--- main_test.py
+++ main_test.py
@@ -7,8 +7,9 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    # datastr = '10\n25\n15\n35\n50'
-    # sys.stdin = io.StringIO(datastr)
+    # datastr = '90\n10\n50\n40\n30'
+    datastr = '90 10 50 40 30'
+    sys.stdin = io.StringIO(datastr)
 
     main.main()
     sys.stdout = sys.__stdout__
@@ -16,15 +17,32 @@ def test_main_1():
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    rdlst = list(map(int, lines[0].split()))
-    print(rdlst)
-    mval = min(rdlst)
-    midx = rdlst.index(mval)
-
-    regex_string = r'[\w,\W]*' + str(mval)
-    regex_string += r'[\w,\W]*' + str(midx)
+    regex_string = r'[\w,\W]*90'
+    regex_string += r'[\w,\W]*50'
     regex_string += r'[\w,\W]*'
     print(regex_string)
-    res = re.search(regex_string, lines[1])
+    res = re.search(regex_string, lines[0])
+    assert res != None
+    print(res.group())
+
+
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    # datastr = '90\n10\n50\n40\n30'
+    datastr = '1 2 3 4 5'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    regex_string = r'[\w,\W]*4'
+    regex_string += r'[\w,\W]*5'
+    regex_string += r'[\w,\W]*'
+    print(regex_string)
+    res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
