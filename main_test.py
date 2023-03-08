008076d4f4aa5867a1ef220643ad22eb370bda38
Kayw Lee <kwooler@gmail.com>
2023-03-07 17:43:19 -0800

    L73

--- main_test.py
+++ main_test.py
@@ -7,8 +7,8 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    # datastr = '90\n10\n50\n40\n30'
-    datastr = '90 10 50 40 30'
+    datastr = '90\n10\n50\n40\n30'
+    # datastr = '90 10 50 40 30'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -29,8 +29,8 @@ def test_main_1():
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    # datastr = '90\n10\n50\n40\n30'
-    datastr = '1 2 3 4 5'
+    datastr = '1\n2\n3\n4\n5'
+    # datastr = '1 2 3 4 5'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
